ae6eedb4e5d55906ceb3df2542ce899ef3e43c06
goeyDev <chgoey@outloog.com>
2025-07-16 09:05:11 +0800

    select payment method

--- db/schema.ts
+++ db/schema.ts
@@ -23,6 +23,7 @@ export const users = pgTable("user", {
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
   address: json("address").$type<ShippingAddress>(),
+  paymentMethod: text("peymentMethod"),
 });
 
 export const accounts = pgTable(
