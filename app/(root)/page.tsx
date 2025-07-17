1b351e47c60aede17b4d17bc7aa7c1c05e7cf3d5
goeyDev <chgoey@outloog.com>
2025-07-17 15:01:00 +0800

    add drawer and carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProduct } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProduct,
+} from "@/lib/actions/product.actions";
 // import sampleData from "@/lib/sample-data";
 
 export default async function Home() {
   const latestProducts = await getLatestProduct();
+  const featuredProducts = await getFeaturedProducts();
   return (
     <div className="space-y-8">
       <ProductList title="Latest Products" data={latestProducts} />
+
+      <div>
+        {featuredProducts.length > 0 && (
+          <ProductCarousel data={featuredProducts} />
+        )}
+        <div className="space-y-8">
+          <ProductList title="Newest Arrivals" data={latestProducts} />
+        </div>
+      </div>
     </div>
   );
 }
